ba6a71fb51c3f9debdab81fb8be5f4b2da06bed8
ereio <Tte11@my.fsu.edu>
2017-02-11 10:14:27 -0500

    updating structure

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ var port = process.env.PORT || 443;
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
-})
+});
+
+app.use('/modules', require('./server/routes/modules'));
+app.use('/fleet', require('./server/routes/admin'));
 
 app.listen(port, function () {
   console.log('Example app listening on port 8080!')
