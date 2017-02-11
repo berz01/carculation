f71ff2dd74ff966d4eb2ee42c6f35503ea2e76e6
ereio <Tte11@my.fsu.edu>
2017-02-11 09:47:05 -0500

    ugh

--- server.js
+++ server.js
@@ -1,11 +1,12 @@
 var express = require('express')
 var app = express()
 
+var port = process.env.PORT || 443;
+
 app.get('/', function (req, res) {
   res.send('Hello World!');
 })
 
-app.listen(8080, function () {
+app.listen(port, function () {
   console.log('Example app listening on port 8080!')
-
 });
