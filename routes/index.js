d8826a186b34aabd5c3cbc47ddd01a3952756a49
ereio <ereio@protonmail.com>
2018-04-15 19:26:13 -0400

    oops

--- routes/index.js
+++ routes/index.js
@@ -21,12 +21,12 @@ api.use(bodyParser.json());
 exports.auth = function(req, res, next) {
   // // {Math.round(distance * .75 * 100)/100}
   const code = req.query.code;
-  //
+
   function saveToken(error, result) {
     if (error) {
       console.log('Access token error', error.message);
       res.send('Access token error: ' + error.message);
-      return;
+      return res.http();
     }
 
     // Attach `token` to the user's session for later use
@@ -40,7 +40,7 @@ exports.auth = function(req, res, next) {
     code: code
   }, saveToken);
 
-  // return res.redirect('http://carculation.diameter.tech.s3-website-us-east-1.amazonaws.com#accessToken=' + req.session.token);
+  return res.redirect('http://carculation.diameter.tech.s3-website-us-east-1.amazonaws.com#accessToken=' + req.session.token);
   // return res.redirect('/home');
 };
 
